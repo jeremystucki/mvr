6ab09bee9fb8fb64df1ba1325f31606fba8dff7a
Jeremy Stucki <jeremy@myelin.ch>
2019-04-07 15:02:14 +0200

    Extract parameter names into constants

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use clap::{App, Arg, crate_version};
+use clap::{crate_version, App, Arg};
+
+const OLD_PATTERN_PARAMETER_NAME: &str = "old pattern";
+const NEW_PATTERN_PARAMETER_NAME: &str = "new pattern";
 
 fn main() {
     let matches = App::new("mvr")
         .author("Jeremy Stucki")
         .version(crate_version!())
         .about("Rename batches of files")
-        .arg(Arg::with_name("old pattern").required(true))
-        .arg(Arg::with_name("new pattern").required(true))
+        .arg(Arg::with_name(OLD_PATTERN_PARAMETER_NAME).required(true))
+        .arg(Arg::with_name(NEW_PATTERN_PARAMETER_NAME).required(true))
         .get_matches();
 }
