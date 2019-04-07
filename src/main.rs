54f0303b3ab9b3a7813aded98dbe46ab98cf8f92
Jeremy Stucki <jeremy@myelin.ch>
2019-04-07 15:52:58 +0200

    Add help text

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,19 @@ fn main() {
         .author("Jeremy Stucki")
         .version(crate_version!())
         .about("Rename batches of files")
-        .arg(Arg::with_name(OLD_PATTERN_PARAMETER_NAME).required(true))
-        .arg(Arg::with_name(NEW_PATTERN_PARAMETER_NAME).required(true))
+        .arg(
+            Arg::with_name(OLD_PATTERN_PARAMETER_NAME)
+                .required(true)
+                .help(
+                    "Use braces to indicate groups\n\
+                     Use question marks to match a single character\n\
+                     Use asterisks to match any amount of characters",
+                ),
+        )
+        .arg(
+            Arg::with_name(NEW_PATTERN_PARAMETER_NAME)
+                .required(true)
+                .help("Use $n to insert a matched group (0-based)"),
+        )
         .get_matches();
 }
