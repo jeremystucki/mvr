7ed142d1dfe5bc6e39bd0a646ab1ab314bd7a61b
Jeremy Stucki <jeremy@myelin.ch>
2019-04-07 19:30:06 +0200

    Work on pattern parsing

--- src/pattern.rs
+++ src/pattern.rs
@@ -19,12 +19,14 @@ struct SearchPatternElement {
 #[derive(Debug)]
 enum PatternError {
     InvalidSyntax,
+    EmptyPattern,
 }
 
 impl Display for PatternError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let message = match self {
             PatternError::InvalidSyntax => "The given pattern is not valid",
+            PatternError::EmptyPattern => "The given pattern is empty",
         };
 
         write!(f, "{}", message)
@@ -70,6 +72,40 @@ impl SearchPatternImpl {
     }
 }
 
+fn get_search_pattern_elements(pattern: &str) -> Result<Vec<Token>, PatternError> {
+    let mut chars = pattern.chars();
+
+    let first_character = chars
+        .next()
+        .into_result()
+        .map_err(PatternError::EmptyPattern)?;
+
+    let tokens = match first_character {
+        '*' => vec![Token::Wildcard].append(&mut get_search_pattern_elements(&pattern[1..])?),
+        '?' => {
+            let end_of_fixed_length_token = chars.position(|character| character != '?');
+
+            match end_of_fixed_length_token {
+                None => vec![Token::FixedLength(pattern.len())],
+                Some(position) => vec![Token::FixedLength(position)]
+                    .append(&mut get_search_pattern_elements(&pattern[position..])?),
+            }
+        }
+        _ => {
+            let end_of_text_token =
+                chars.position(|character| character == '?' || character == '*');
+
+            match end_of_text_token {
+                None => vec![Token::Text(first_character.to_string())],
+                Some(position) => vec![Token::Text(pattern[..position].to_string())]
+                    .append(&mut get_search_pattern_elements(&pattern[position..])?),
+            }
+        }
+    };
+
+    Ok(tokens)
+}
+
 impl SearchPattern for SearchPatternImpl {
     fn match_against(&self, query: &str) -> Result<Vec<CaptureGroup>, MatchingError> {
         unimplemented!()
@@ -108,5 +144,10 @@ mod tests {
     #[test]
     fn pattern_with_mismatched_braces_is_invalid() {
         SearchPatternImpl::try_new("foo(.bar").unwrap_err();
+    }
+
+    #[test]
+    fn empty_pattern_is_invalid() {
+        SearchPatternImpl::try_new("").unwrap_err();
     }
 }
