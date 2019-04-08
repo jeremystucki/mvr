45fd159fb4b1b1dfe2d7d9e059d75643b7cedc49
Jeremy Stucki <stucki.jeremy@gmail.com>
2019-04-08 15:39:12 +0200

    Write tests

--- src/matching_pattern.rs
+++ src/matching_pattern.rs
@@ -2,25 +2,25 @@ use std::error::Error;
 use std::fmt::{self, Display};
 use std::num::NonZeroUsize;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Token {
     Text(String),
     FixedLength(NonZeroUsize),
     Wildcard,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Element {
     Token(Token),
     Group(Vec<Token>),
 }
 
-#[derive(Debug)]
-struct MatchingPattern {
+#[derive(Debug, PartialEq)]
+struct Pattern {
     elements: Vec<Element>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum ParsingError {
     InvalidSyntax,
 }
@@ -37,14 +37,108 @@ impl Display for ParsingError {
 
 impl Error for ParsingError {}
 
-trait MatchingPatternParser {
-    fn parse(&self, input: &str) -> Result<MatchingPattern, ParsingError>;
+trait Parser {
+    fn parse(&self, input: &str) -> Result<Pattern, ParsingError>;
 }
 
-struct ReplacementPatternParserImpl {}
+struct ParserImpl {}
 
-impl MatchingPatternParser for ReplacementPatternParserImpl {
-    fn parse(&self, input: &str) -> Result<MatchingPattern, ParsingError> {
+impl ParserImpl {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Parser for ParserImpl {
+    fn parse(&self, input: &str) -> Result<Pattern, ParsingError> {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_with_wildcard_pattern() {
+        let expected = Pattern {
+            elements: vec![
+                Element::Token(Token::Text(String::from("foo"))),
+                Element::Token(Token::Wildcard),
+                Element::Token(Token::Text(String::from("bar"))),
+            ],
+        };
+
+        let actual = ParserImpl::new().parse("foo*.bar").unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_with_wildcard_pattern_inside_capture_group() {
+        let expected = Pattern {
+            elements: vec![
+                Element::Token(Token::Text(String::from("foo"))),
+                Element::Group(vec![Token::Wildcard]),
+                Element::Token(Token::Text(String::from("bar"))),
+            ],
+        };
+
+        let actual = ParserImpl::new().parse("foo(*).bar").unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_with_fixed_length_pattern() {
+        let expected = Pattern {
+            elements: vec![
+                Element::Token(Token::Text(String::from("foo_"))),
+                Element::Token(Token::FixedLength(NonZeroUsize::new(2).unwrap())),
+                Element::Token(Token::Text(String::from("bar"))),
+            ],
+        };
+
+        let actual = ParserImpl::new().parse("foo_??.bar").unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_with_text_only() {
+        let expected = Pattern {
+            elements: vec![Element::Token(Token::Text(String::from("foo.bar")))],
+        };
+
+        let actual = ParserImpl::new().parse("foo.bar").unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_with_wildcard_and_fixed_length_token_in_capture_group() {
+        let expected = Pattern {
+            elements: vec![
+                Element::Token(Token::Text(String::from("foo_"))),
+                Element::Group(vec![
+                    Token::FixedLength(NonZeroUsize::new(2).unwrap()),
+                    Token::Text(String::from(".")),
+                    Token::Wildcard,
+                ]),
+            ],
+        };
+
+        let actual = ParserImpl::new().parse("foo_(??.*)").unwrap();
+
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parse_fails_with_mismatched_grouping() {
+        let expected = ParsingError::InvalidSyntax;
+
+        let actual = ParserImpl::new().parse("foo_(??.*").unwrap_err();
+
+        assert_eq!(expected, actual);
     }
 }
